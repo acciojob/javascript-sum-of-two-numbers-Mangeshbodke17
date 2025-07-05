02d9773c0695bbc25bd62d29dc7c76c0c65bae17
Mangesh Bodke <mangeshbodke2025@gmail.com>
2025-07-05 22:42:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ let input1 = prompt("Enter the first number:");
 let input2 = prompt("Enter the second number:");
 
 // Write your code here and print the output using alert function
+
+let c=parseInt(input1)+parseInt(input2);
+
+if(isNaN(c)){
+console.log("Invalid input. Please enter a valid number.");
+}
+else{
+console.log(c);
+}
+
